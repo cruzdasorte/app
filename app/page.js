3894d00577ca1bc7757505763c4cf18f493805a3
hrm4k3r <hebertdev82@gmail.com>
2025-02-25 16:32:50 -0300

    feat: new details

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
-      <h1 className="font-bold text-4xl text-center">Cruz da Sorte</h1>
-    </>
+    <div>
+      <h2 className="font-bold text-4xl text-center justify-center">Cruz da Sorte</h2>
+      <h3 className="mb-10">Em desenvolvimento</h3>
+      <p>Desenvolvido por hrm4k3r, da empresa Rixxer</p>
+    </div>
   );
 }
