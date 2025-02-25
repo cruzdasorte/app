470dc247a451b7e35248c62da3d5a0ddfee18c5c
hrm4k3r <hebertdev82@gmail.com>
2025-02-25 16:34:44 -0300

    feat: new details

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h2 className="font-bold text-4xl text-center justify-center">Cruz da Sorte</h2>
-      <h3 className="mb-10">Em desenvolvimento</h3>
-      <p>Desenvolvido por hrm4k3r, da empresa Rixxer</p>
+      <h2 className="font-bold text-4xl text-center justify-center align-center">Cruz da Sorte</h2>
+      <h3 className="text-xl font-bold mb-10 text-center justify-center align-center">Em desenvolvimento</h3>
+      <p className="font-bold text-center justify-center align-center">Desenvolvido por hrm4k3r, da empresa Rixxer</p>/
     </div>
   );
 }
